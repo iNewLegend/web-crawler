45b424e84e93ab553eb437ad229a0e3c72801c64
Leonid Vinikov <czf.leo123@gmail.com>
2023-09-01 21:37:34 +0300

    New: ClawlerBackend - POC

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 -  2023-08-31 08:00:00 ~ 14:30:00 `New: CrawlerLib - Initial Commit`
 -  2023-08-31 14:30:00 ~ 16:00:00 `Internal: Workflows - Add test workflow`
 -  2023-08-31 `Tweak: CrawlerLib - Add memory cache`
+-  2023-09-01 `New: ClawlerBackend - POC`
--- crawler-backend/routes/api.php
+++ crawler-backend/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/crawler', \App\Http\Controllers\CrawlerController::class)
+    ->middleware(\App\Http\Middleware\CrawlerNormalizer::class);
